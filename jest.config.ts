78bd3200f7880054fe687868ddfce0d355f0eebd
Yorí Galisteu <yorigcsdev@gmail.com>
2023-01-03 00:43:27 -0300

    chore: add config files

---
+++ jest.config.ts
@@ -0,0 +1,23 @@
+export default {
+  clearMocks: true,
+  collectCoverageFrom: [
+    '<rootDir>/src/**/*.ts',
+    '!<rootDir>/src/main/**',
+    '!<rootDir>/src/**/index.ts'
+  ],
+  coverageDirectory: 'coverage',
+  coverageProvider: 'babel',
+  moduleNameMapper: {
+    '@/tests/(.+)': '<rootDir>/tests/$1',
+    '@/(.+)': '<rootDir>/src/$1'
+  },
+  roots: [
+    '<rootDir/src',
+    '<rootDir>/tests'
+  ],
+  setupFiles: ['dotenv/config'],
+  testMatch: ['**/*.spec.ts'],
+  transform: {
+    '\\.ts$': 'ts-jest'
+  }
+}
