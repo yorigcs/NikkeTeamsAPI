151a234f51e71d9e337c13d58f09c2de583f5e26
Yorí Galisteu <yorigcsdev@gmail.com>
2023-01-10 20:38:16 -0300

    test: ensure AddAccountService returns account info if sucess or AddAccountError if Encrypter throws

--- tests/data/services/add-account.spec.ts
+++ tests/data/services/add-account.spec.ts
@@ -2,6 +2,7 @@
 import { randomUUID } from 'crypto'
 import { Encrypter } from '@/data/contracts/crypto'
 import { AddAccountService } from '@/data/services'
+import { AddAccountError } from '@/domain/errors'
 
 class EncrypterSpy implements Encrypter {
   plainText?: string
@@ -20,15 +21,22 @@ describe('AddAccountService', () => {
   it('should call Encrypter with correct params', async () => {
     const encryptSpy = new EncrypterSpy()
     const sut = new AddAccountService(encryptSpy)
-    await sut.perform({ email: 'any@mail.com', name: 'any_name', password: 'any_password' })
+    await sut.perform({ email: 'any@mail.com', name: 'any_name', password: 'any_password', picture: 'any_picture' })
     expect(encryptSpy.plainText).toBe('any_password')
   })
 
-  it('should throws if Encrypter throws', async () => {
+  it('should returns AddAccountError if Encrypter throws', async () => {
     const encryptSpy = new EncrypterSpy()
     const sut = new AddAccountService(encryptSpy)
     jest.spyOn(encryptSpy, 'encrypt').mockImplementationOnce(throwError)
-    const promise = sut.perform({ email: 'any@mail.com', name: 'any_name', password: 'any_password' })
-    await expect(promise).rejects.toThrow()
+    const promise = await sut.perform({ email: 'any@mail.com', name: 'any_name', password: 'any_password', picture: 'any_picture' })
+    expect(promise).toBeInstanceOf(AddAccountError)
+  })
+
+  it('should returns account infos without password if sucess', async () => {
+    const encryptSpy = new EncrypterSpy()
+    const sut = new AddAccountService(encryptSpy)
+    const promise = await sut.perform({ email: 'any@mail.com', name: 'any_name', password: 'any_password', picture: 'any_picture' })
+    expect(promise).toEqual({ email: 'any@mail.com', name: 'any_name', picture: 'any_picture' })
   })
 })
