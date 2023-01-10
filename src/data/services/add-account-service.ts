121aa283c75a9fb1eb7b021754079116782afa6f
Yorí Galisteu <yorigcsdev@gmail.com>
2023-01-10 20:29:45 -0300

    feat: returns account info if sucess or AddAccountError if Encrypter throws

--- src/data/services/add-account-service.ts
+++ src/data/services/add-account-service.ts
@@ -1,9 +1,17 @@
 import { Encrypter } from '@/data/contracts/crypto'
+import { AddAccoutError } from '@/domain/errors'
 import { AddAcount } from '@/domain/feature'
+
 export class AddAccountService {
   constructor (private readonly encrypter: Encrypter) { }
 
-  async perform (params: AddAcount.Params): Promise<void> {
-    await this.encrypter.encrypt({ plainText: params.password })
+  async perform (params: AddAcount.Params): Promise<AddAcount.Result> {
+    try {
+      await this.encrypter.encrypt({ plainText: params.password })
+      const { password, ...accountInfo } = params
+      return { ...accountInfo }
+    } catch (err) {
+      return new AddAccoutError()
+    }
   }
 }
