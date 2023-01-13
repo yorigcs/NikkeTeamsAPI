08c02867ea2334f712d8fd5d430679b5aace33f2
Yorí Galisteu <yorigcsdev@gmail.com>
2023-01-13 00:47:20 -0300

    feat: add SaveAccountRepository to AddAccountService constructor

--- src/data/services/add-account-service.ts
+++ src/data/services/add-account-service.ts
@@ -1,14 +1,20 @@
 import { Encrypter } from '@/data/contracts/crypto'
 import { AddAccountError } from '@/domain/errors'
 import { AddAcount } from '@/domain/feature'
+import { SaveAccountRepository } from '@/data/contracts/repo/user-account'
 
 export class AddAccountService {
-  constructor (private readonly encrypter: Encrypter) { }
+  constructor (
+    private readonly encrypter: Encrypter,
+    private readonly saveAccountRepo: SaveAccountRepository
+  ) { }
 
   async perform (params: AddAcount.Params): Promise<AddAcount.Result> {
     try {
-      await this.encrypter.encrypt({ plainText: params.password })
       const { password, ...accountInfo } = params
+      const hashpassword = await this.encrypter.encrypt({ plainText: password })
+      await this.saveAccountRepo.save({ id: 'any_id', password: hashpassword, ...accountInfo })
+
       return { ...accountInfo }
     } catch (err) {
       return new AddAccountError()
