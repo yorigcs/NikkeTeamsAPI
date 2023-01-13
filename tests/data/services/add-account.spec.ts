0dafaca8a5c520b273a84e6556c3f5f4363a037a
Yorí Galisteu <yorigcsdev@gmail.com>
2023-01-13 00:48:49 -0300

    test: ensure saveAccountRepo is called correctly

--- tests/data/services/add-account.spec.ts
+++ tests/data/services/add-account.spec.ts
@@ -1,34 +1,54 @@
-import { Encrypter } from '@/data/contracts/crypto'
-import { AddAccountService } from '@/data/services'
 import { AddAccountError } from '@/domain/errors'
 
-import { mock } from 'jest-mock-extended'
+import { AddAccountService } from '@/data/services'
+import { Encrypter } from '@/data/contracts/crypto'
+import { SaveAccountRepository } from '@/data/contracts/repo'
+
+import { mock, MockProxy } from 'jest-mock-extended'
 
 const throwError = (): never => {
   throw new Error()
 }
 
 describe('AddAccountService', () => {
+  let encrypter: MockProxy<Encrypter>
+  let saveAccountRepo: MockProxy<SaveAccountRepository>
+  let sut: AddAccountService
+  const accountData = { email: 'any@mail.com', name: 'any_name', password: 'any_password', picture: 'any_picture' }
+
+  beforeEach(() => {
+    encrypter = mock()
+    encrypter.encrypt.mockResolvedValue('hashedPassword')
+    saveAccountRepo = mock()
+    sut = new AddAccountService(encrypter, saveAccountRepo)
+  })
+
   it('should call Encrypter with correct params', async () => {
-    const encrypter = mock<Encrypter>()
-    const sut = new AddAccountService(encrypter)
-    await sut.perform({ email: 'any@mail.com', name: 'any_name', password: 'any_password', picture: 'any_picture' })
+    await sut.perform(accountData)
     expect(encrypter.encrypt).toHaveBeenCalledWith({ plainText: 'any_password' })
     expect(encrypter.encrypt).toHaveBeenCalledTimes(1)
   })
 
   it('should returns AddAccountError if Encrypter throws', async () => {
-    const encrypter = mock<Encrypter>()
-    const sut = new AddAccountService(encrypter)
-    jest.spyOn(encrypter, 'encrypt').mockImplementationOnce(throwError)
-    const promise = await sut.perform({ email: 'any@mail.com', name: 'any_name', password: 'any_password', picture: 'any_picture' })
+    encrypter.encrypt.mockImplementationOnce(throwError)
+    const promise = await sut.perform(accountData)
     expect(promise).toBeInstanceOf(AddAccountError)
   })
 
+  it('should returns AddAccountError if CreateUserAccount throws', async () => {
+    saveAccountRepo.save.mockImplementationOnce(throwError)
+    const promise = await sut.perform(accountData)
+    expect(promise).toBeInstanceOf(AddAccountError)
+  })
+
+  it('should call saveAccountRepo with correct params', async () => {
+    await sut.perform(accountData)
+    expect(saveAccountRepo.save).toHaveBeenCalledWith({ id: 'any_id', ...accountData, password: 'hashedPassword' })
+    expect(saveAccountRepo.save).toHaveBeenCalledTimes(1)
+  })
+
   it('should returns account infos without password if sucess', async () => {
-    const encrypter = mock<Encrypter>()
-    const sut = new AddAccountService(encrypter)
-    const promise = await sut.perform({ email: 'any@mail.com', name: 'any_name', password: 'any_password', picture: 'any_picture' })
+    const promise = await sut.perform(accountData)
     expect(promise).toEqual({ email: 'any@mail.com', name: 'any_name', picture: 'any_picture' })
   })
 })
