8050d6f50c85dff42f17ab83dc7ebb278ac1adf4
Yorí Galisteu <yorigcsdev@gmail.com>
2023-01-12 22:59:10 -0300

    refactor: change tests to use jest-mock-extend

--- tests/data/services/add-account.spec.ts
+++ tests/data/services/add-account.spec.ts
@@ -1,17 +1,8 @@
-
-import { randomUUID } from 'crypto'
 import { Encrypter } from '@/data/contracts/crypto'
 import { AddAccountService } from '@/data/services'
 import { AddAccountError } from '@/domain/errors'
 
-class EncrypterSpy implements Encrypter {
-  plainText?: string
-  cipherText = randomUUID()
-  async encrypt (params: Encrypter.Params): Promise<string> {
-    this.plainText = params.plainText
-    return this.cipherText
-  }
-}
+import { mock } from 'jest-mock-extended'
 
 const throwError = (): never => {
   throw new Error()
@@ -19,23 +10,24 @@ const throwError = (): never => {
 
 describe('AddAccountService', () => {
   it('should call Encrypter with correct params', async () => {
-    const encryptSpy = new EncrypterSpy()
-    const sut = new AddAccountService(encryptSpy)
+    const encrypter = mock<Encrypter>()
+    const sut = new AddAccountService(encrypter)
     await sut.perform({ email: 'any@mail.com', name: 'any_name', password: 'any_password', picture: 'any_picture' })
-    expect(encryptSpy.plainText).toBe('any_password')
+    expect(encrypter.encrypt).toHaveBeenCalledWith({ plainText: 'any_password' })
+    expect(encrypter.encrypt).toHaveBeenCalledTimes(1)
   })
 
   it('should returns AddAccountError if Encrypter throws', async () => {
-    const encryptSpy = new EncrypterSpy()
-    const sut = new AddAccountService(encryptSpy)
-    jest.spyOn(encryptSpy, 'encrypt').mockImplementationOnce(throwError)
+    const encrypter = mock<Encrypter>()
+    const sut = new AddAccountService(encrypter)
+    jest.spyOn(encrypter, 'encrypt').mockImplementationOnce(throwError)
     const promise = await sut.perform({ email: 'any@mail.com', name: 'any_name', password: 'any_password', picture: 'any_picture' })
     expect(promise).toBeInstanceOf(AddAccountError)
   })
 
   it('should returns account infos without password if sucess', async () => {
-    const encryptSpy = new EncrypterSpy()
-    const sut = new AddAccountService(encryptSpy)
+    const encrypter = mock<Encrypter>()
+    const sut = new AddAccountService(encrypter)
     const promise = await sut.perform({ email: 'any@mail.com', name: 'any_name', password: 'any_password', picture: 'any_picture' })
     expect(promise).toEqual({ email: 'any@mail.com', name: 'any_name', picture: 'any_picture' })
   })
