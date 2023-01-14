99956822fe67d21afa1e90756fe663cdefa226a3
Yorí Galisteu <yorigcsdev@gmail.com>
2023-01-14 02:24:24 -0300

    test: ensure UUIDHandler with v4 returns correctly

---
+++ tests/infra/crypto/uuid.spec.ts
@@ -0,0 +1,33 @@
+
+import { v4 } from 'uuid'
+import { UUIDHandler } from '@/infra/crypto'
+
+jest.mock('uuid')
+
+describe('UuidHandler', () => {
+  let sut: UUIDHandler
+
+  beforeAll(() => {
+    jest.mocked(v4).mockReturnValue('any_uuid')
+  })
+
+  beforeEach(() => {
+    sut = new UUIDHandler()
+  })
+
+  it('should call v4', async () => {
+    await sut.generate({ key: 'any_key' })
+    expect(v4).toHaveBeenCalledTimes(1)
+  })
+
+  it('should returns correct uuid', async () => {
+    const uuid = await sut.generate({ key: 'any_key' })
+    expect(uuid).toBe('any_key_any_uuid')
+  })
+
+  it('throws if uuid is not a string', async () => {
+    jest.mocked(v4).mockImplementationOnce(() => { throw new Error('uuid is not a string') })
+    const promise = sut.generate({ key: 'any_key' })
+    await expect(promise).rejects.toThrow(new Error('uuid is not a string'))
+  })
+})
