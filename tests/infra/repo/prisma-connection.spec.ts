97b6a860b06ba4a2725e9637079789f900bc270e
Yorí Galisteu <yorigcsdev@gmail.com>
2023-01-16 16:30:07 -0300

    test: ensure prismaConnection is instanceOf prismaClient

--- tests/infra/repo/prisma-connection.spec.ts
+++ tests/infra/repo/prisma-connection.spec.ts
@@ -1,6 +1,7 @@
 import { PrismaClient } from '@prisma/client'
-import prismaConnection from '@/infra/repo/helpers/prisma'
+import prismaConnection from '@/infra/repo/prisma'
 
+jest.unmock('@/infra/repo/prisma')
 describe('prismaConnection', () => {
   it('should be instance of PrismaClient', () => {
     expect(prismaConnection).toBeInstanceOf(PrismaClient)
