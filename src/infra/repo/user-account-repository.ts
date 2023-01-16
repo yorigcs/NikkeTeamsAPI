152d43c6637ec13702b5ded1b32fe58dc6c7b6ff
Yorí Galisteu <yorigcsdev@gmail.com>
2023-01-16 17:10:21 -0300

    feat: implements SaveAccountRepository

--- src/infra/repo/user-account-repository.ts
+++ src/infra/repo/user-account-repository.ts
@@ -1,8 +1,12 @@
-import { LoadAccountByEmailRepository } from '@/data/contracts/repo'
+import { LoadAccountByEmailRepository, SaveAccountRepository } from '@/data/contracts/repo'
 import prismaConnection from '@/infra/repo/prisma'
 
-export class UserAccountRepository implements LoadAccountByEmailRepository {
+export class UserAccountRepository implements LoadAccountByEmailRepository, SaveAccountRepository {
   async load ({ email }: LoadAccountByEmailRepository.Input): Promise<LoadAccountByEmailRepository.Output> {
     return await prismaConnection.users.findUnique({ where: { email } }) !== null
+  }
+
+  async save (input: SaveAccountRepository.Input): Promise<void> {
+    await prismaConnection.users.create({ data: input })
   }
 }
