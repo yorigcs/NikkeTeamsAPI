eb7f231b6909426be7a9f6443759991ee748272a
Yorí Galisteu <yorigcsdev@gmail.com>
2023-01-15 21:46:36 -0300

    refactor: remove unnecessary logic

--- src/infra/crypto/uuid-handler.ts
+++ src/infra/crypto/uuid-handler.ts
@@ -4,9 +4,6 @@ import { v4 } from 'uuid'
 export class UUIDHandler implements UUID {
   async generate ({ key }: UUID.Input): Promise<UUID.Output> {
     const uuid = v4()
-    if (typeof uuid === 'string') {
-      return `${key}_${uuid}`
-    }
-    throw new Error()
+    return `${key}_${uuid}`
   }
 }
