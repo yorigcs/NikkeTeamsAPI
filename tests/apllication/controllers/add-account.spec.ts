f8ec66d56764da02c5c6943142136d469e6cd393
Yorí Galisteu <yorigcsdev@gmail.com>
2023-01-25 19:43:09 -0300

    test: should returns statusCode 422 if no password is provided

--- tests/apllication/controllers/add-account.spec.ts
+++ tests/apllication/controllers/add-account.spec.ts
@@ -40,4 +40,10 @@ describe('AddAccountController', () => {
     const httpResponse = await sut.handle(httpRequest)
     expect(httpResponse).toEqual({ statusCode: 422, body: { error: new Error('The field email is required') } })
   })
+
+  it('should returns status code 422 if no password email provided', async () => {
+    httpRequest.password = undefined
+    const httpResponse = await sut.handle(httpRequest)
+    expect(httpResponse).toEqual({ statusCode: 422, body: { error: new Error('The field password is required') } })
+  })
 })
