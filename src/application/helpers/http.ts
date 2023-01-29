8c49a096b1fa203b475718483ac64579b6136cc7
Yorí Galisteu <yorigcsdev@gmail.com>
2023-01-28 22:13:22 -0300

    feat: create serverError helper

--- src/application/helpers/http.ts
+++ src/application/helpers/http.ts
@@ -1,4 +1,13 @@
-export type HttpResponse = {
+import { ServerError } from '@/application/errors'
+
+export type HttpResponse<T = any> = {
   statusCode: number
-  body: any
+  data: T
 }
+
+export const serverError = (error: Error): HttpResponse<Error> => (
+  {
+    statusCode: 500,
+    data: new ServerError(error)
+  }
+)
