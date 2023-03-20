ee1d6247899787ae8ec7fd8913a9e5a04e8402fd
Yorí Galisteu <yorigcsdev@gmail.com>
2023-03-20 00:57:11 -0300

    test: should throws if sign throws

--- tests/infra/crypto/jwt-token-handler.spec.ts
+++ tests/infra/crypto/jwt-token-handler.spec.ts
@@ -30,12 +30,22 @@ describe('', () => {
 
   it('should calls sign with correct params', async () => {
     await sut.generate({ key, expirationInMs })
+
     expect(sign).toHaveBeenCalledTimes(1)
     expect(sign).toHaveBeenCalledWith({ key }, secret, { expiresIn: 1 })
   })
 
   it('should returns a token', async () => {
     const resp = await sut.generate({ key, expirationInMs })
+
     expect(resp).toBe('any_token')
+  })
+
+  it('should throws if sign throws', async () => {
+    jest.mocked(sign).mockImplementationOnce(() => { throw new Error('token_error') })
+
+    const resp = sut.generate({ key, expirationInMs })
+
+    await expect(resp).rejects.toThrow(new Error('token_error'))
   })
 })
