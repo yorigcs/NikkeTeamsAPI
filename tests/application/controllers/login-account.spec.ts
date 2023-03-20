45f7b5746dd73f939ab3b76d8d4105cca061cfc3
Yorí Galisteu <yorigcsdev@gmail.com>
2023-03-20 17:23:36 -0300

    test: ensure loginAccount is called with with correct input

--- tests/application/controllers/login-account.spec.ts
+++ tests/application/controllers/login-account.spec.ts
@@ -1,17 +1,41 @@
 import { Controller } from '@/application/controllers'
-import { HttpResponse } from '../helpers'
+import { LoginAccount } from '@/domain/use-cases'
+import { HttpResponse, ok } from '@/application/helpers'
 
+type HttpRequest = { email: string, password: string }
 class LoginAccountController extends Controller {
-  async perform (httpRequest: any): Promise<HttpResponse<any>> {
-    throw new Error('Method not implemented.')
+  constructor (private readonly loginAccount: LoginAccount) {
+    super()
+  }
+
+  async perform ({ email, password }: HttpRequest): Promise<HttpResponse<any>> {
+    await this.loginAccount({ email, password })
+    return ok('any_data')
   }
 }
+
 describe('LoginAccountController', () => {
+  const data = {
+    user: { name: 'any_name', email: 'any@mail', picture: 'any_picture', role: 'user' },
+    acessToken: 'any_token',
+    refreshToken: 'any_token'
+  }
   let sut: LoginAccountController
-  beforeEach(() => {
-    sut = new LoginAccountController()
+  let loginAccount: jest.Mock
+
+  beforeAll(() => {
+    loginAccount = jest.fn()
+    loginAccount.mockReturnValue(data)
   })
+  beforeEach(() => { sut = new LoginAccountController(loginAccount) })
+
   it('should extends controller', () => {
     expect(sut).toBeInstanceOf(Controller)
+  })
+
+  it('should calls loginAccount with correct input', async () => {
+    await sut.perform({ email: 'any@mail', password: 'any_password' })
+    expect(loginAccount).toHaveBeenCalledTimes(1)
+    expect(loginAccount).toHaveBeenCalledWith({ email: 'any@mail', password: 'any_password' })
   })
 })
