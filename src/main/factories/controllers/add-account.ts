517431d3258a869d3565853289c3e4a3c8c498af
Yorí Galisteu <yorigcsdev@gmail.com>
2023-03-20 13:03:44 -0300

    refactor: add makeLoginAccountUseCase

--- src/main/factories/controllers/add-account.ts
+++ src/main/factories/controllers/add-account.ts
@@ -1,6 +1,6 @@
 import { AddAccountController } from '@/application/controllers'
-import { makeAddAccountUseCase } from '@/main/factories/use-cases'
+import { makeAddAccountUseCase, makeLoginAccountUseCase } from '@/main/factories/use-cases'
 
 export const makeAddAccountController = (): AddAccountController => {
-  return new AddAccountController(makeAddAccountUseCase())
+  return new AddAccountController(makeAddAccountUseCase(), makeLoginAccountUseCase())
 }
