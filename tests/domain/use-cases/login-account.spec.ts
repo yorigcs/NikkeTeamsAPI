8aa69d66fc82ac9e9ba70ead79c06033a60871c3
Yorí Galisteu <yorigcsdev@gmail.com>
2023-03-19 22:45:39 -0300

    test: ensure user account load method with email is called with correct params

--- tests/domain/use-cases/login-account.spec.ts
+++ tests/domain/use-cases/login-account.spec.ts
@@ -1,18 +1,29 @@
+import { mock, MockProxy } from 'jest-mock-extended'
+import { LoadAccountByEmailRepository } from '../contracts/repo'
 
-type LoginAccount = (params: { email: string, password: string }) => Promise<boolean>
-type Setup = () => LoginAccount
+type LoginAccount = (params: { email: string, password: string }) => Promise<null | string>
+type Setup = (userAccountRepo: LoadAccountByEmailRepository) => LoginAccount
 
-const setupLoginAccount: Setup = () => async params => {
-  return true
+const setupLoginAccount: Setup = (userAccountRepo) => async params => {
+  const { email } = params
+  const user = await userAccountRepo.load({ email })
+  if (user === null) return null
+  return `${user.name}`
 }
 describe('LoginAccountUseCase', () => {
   let sut: LoginAccount
+  let userAccountRepo: MockProxy<LoadAccountByEmailRepository>
 
-  beforeEach(() => {
-    sut = setupLoginAccount()
+  beforeAll(() => {
+    userAccountRepo = mock()
+    userAccountRepo.load.mockResolvedValue(null)
   })
-  it('should returns true when calls sut', async () => {
-    const resp = await sut({ email: 'any@mail', password: 'any_password' })
-    expect(resp).toBe(true)
+  beforeEach(() => {
+    sut = setupLoginAccount(userAccountRepo)
+  })
+  it('should calls user account load method with email', async () => {
+    await sut({ email: 'any@mail', password: 'any_password' })
+    expect(userAccountRepo.load).toHaveBeenLastCalledWith({ email: 'any@mail' })
+    expect(userAccountRepo.load).toHaveBeenCalledTimes(1)
   })
 })
