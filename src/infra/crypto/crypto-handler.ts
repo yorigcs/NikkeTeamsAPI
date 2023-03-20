3f245c76a168e8744d85e02a15a3524f0bd68dd9
Yorí Galisteu <yorigcsdev@gmail.com>
2023-03-20 03:58:58 -0300

    feat: add compare implementation

--- src/infra/crypto/crypto-handler.ts
+++ src/infra/crypto/crypto-handler.ts
@@ -1,9 +1,13 @@
-import { Hasher } from '@/domain/contracts/crypto'
+import { Hasher, HasherCompare } from '@/domain/contracts/crypto'
 import bcrypt from 'bcrypt'
 
-export class HashHandler implements Hasher {
+export class HashHandler implements Hasher, HasherCompare {
   async hash ({ plainText }: Hasher.Input): Promise<Hasher.Output> {
     const salt = 12
     return await bcrypt.hash(plainText, salt)
+  }
+
+  async compare ({ plainText, digest }: HasherCompare.Input): Promise<HasherCompare.Output> {
+    return await bcrypt.compare(plainText, digest)
   }
 }
--- tests/infra/crypto/crypto-handler.spec.ts
+++ tests/infra/crypto/crypto-handler.spec.ts
@@ -1,32 +1,62 @@
 import { HashHandler } from '@/infra/crypto'
-import { hash } from 'bcrypt'
+import { hash, compare } from 'bcrypt'
 
 jest.mock('bcrypt')
 
 describe('HashHandler', () => {
   let sut: HashHandler
 
-  beforeAll(() => {
-    jest.mocked(hash).mockImplementation(() => 'hashedText')
-  })
-
   beforeEach(() => {
     sut = new HashHandler()
   })
 
-  it('should call hash', async () => {
-    await sut.hash({ plainText: 'plainText' })
-    expect(hash).toHaveBeenCalledTimes(1)
+  describe('hash', () => {
+    beforeAll(() => {
+      jest.mocked(hash).mockImplementation(() => 'hashedText')
+    })
+    it('should call hash', async () => {
+      await sut.hash({ plainText: 'plainText' })
+      expect(hash).toHaveBeenCalledTimes(1)
+      expect(hash).toHaveBeenCalledWith('plainText', 12)
+    })
+
+    it('should return a hashedText', async () => {
+      const hashedText = await sut.hash({ plainText: 'plainText' })
+      expect(hashedText).toBe('hashedText')
+    })
+
+    it('should throws an error if hash throws', async () => {
+      jest.mocked(hash).mockImplementationOnce(() => { throw new Error('hash throws an error') })
+      const promise = sut.hash({ plainText: 'plainText' })
+      await expect(promise).rejects.toThrow(new Error('hash throws an error'))
+    })
   })
 
-  it('should return a hashedText', async () => {
-    const hashedText = await sut.hash({ plainText: 'plainText' })
-    expect(hashedText).toBe('hashedText')
-  })
+  describe('compare', () => {
+    beforeAll(() => {
+      jest.mocked(compare).mockImplementation(() => true)
+    })
+    it('should call compare with correct input', async () => {
+      await sut.compare({ plainText: 'any_plainText', digest: 'any_digest' })
+      expect(compare).toHaveBeenCalledTimes(1)
+      expect(compare).toHaveBeenCalledWith('any_plainText', 'any_digest')
+    })
 
-  it('should throws an error if hash throws', async () => {
-    jest.mocked(hash).mockImplementationOnce(() => { throw new Error('hash throws an error') })
-    const promise = sut.hash({ plainText: 'plainText' })
-    await expect(promise).rejects.toThrow(new Error('hash throws an error'))
+    it('should return true if compare returns true', async () => {
+      const valid = await sut.compare({ plainText: 'any_plainText', digest: 'any_digest' })
+      expect(valid).toBe(true)
+    })
+
+    it('should return false if compare returns false', async () => {
+      jest.mocked(compare).mockImplementationOnce(() => false)
+      const valid = await sut.compare({ plainText: 'any_plainText', digest: 'any_digest' })
+      expect(valid).toBe(false)
+    })
+
+    it('should throws an error if compare throws', async () => {
+      jest.mocked(compare).mockImplementationOnce(() => { throw new Error('compare throws an error') })
+      const valid = sut.compare({ plainText: 'any_plainText', digest: 'any_digest' })
+      await expect(valid).rejects.toThrow(new Error('compare throws an error'))
+    })
   })
 })
