c8e2381a6fe8998c2cba551ce683fcc237d707ac
Yorí Galisteu <yorigcsdev@gmail.com>
2023-03-20 23:10:52 -0300

    refactor: change route name to users

--- src/main/routes/users.ts
+++ src/main/routes/users.ts
@@ -1,7 +1,8 @@
 import { adapterExpressController as adapt } from '@/infra/http'
 import { Router } from 'express'
-import { makeAddAccountController } from '../factories/controllers'
+import { makeAddAccountController, makeLoginAccountController } from '../factories/controllers'
 
 export default (router: Router): void => {
   router.post('/sign-up', adapt(makeAddAccountController()))
+  router.post('/sign-in', adapt(makeLoginAccountController()))
 }
