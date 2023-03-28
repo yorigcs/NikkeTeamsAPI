66bec028c16f4250fa729f1e8c340ab050cba03b
Yorí Galisteu <yorigcsdev@gmail.com>
2023-03-28 16:13:02 -0300

    test: ensure returns statusCode 401 if acessToken is empty

--- tests/application/middlewares/authentication.spec.ts
+++ tests/application/middlewares/authentication.spec.ts
@@ -1,5 +1,6 @@
 import { ok, unauthorized, type HttpResponse } from '@/application/helpers'
 import { UnauthorizedError } from '@/application/errors'
+import { RequiredStringValidator } from '@/application/validations'
 
 type Model = Error | { userId: string }
 type HttpRequest = { acessToken: string }
@@ -9,12 +10,18 @@ class AuthenticationMiddleware {
   constructor (private readonly auth: Authorization) {}
 
   async handle ({ acessToken }: HttpRequest): Promise<HttpResponse<Model>> {
+    if (!this.validate({ acessToken })) return unauthorized('acessToken is required')
     try {
       const userId = await this.auth({ token: acessToken })
       return ok({ userId })
     } catch {
       return unauthorized('Invalid token')
     }
+  }
+
+  private validate ({ acessToken }: HttpRequest): boolean {
+    const error = new RequiredStringValidator('acessToken', acessToken).validate()
+    return error === undefined
   }
 }
 
@@ -46,5 +53,10 @@ describe('AuthenticationMiddleware', () => {
     authorization.mockRejectedValueOnce(new Error('Invalid token'))
     const httpResponse = await sut.handle({ acessToken })
     expect(httpResponse).toEqual({ statusCode: 401, data: new UnauthorizedError('Invalid token') })
+  })
+
+  it('should returns status code 401 if acessToken is  empty', async () => {
+    const httpResponse = await sut.handle({ acessToken: '' })
+    expect(httpResponse).toEqual({ statusCode: 401, data: new UnauthorizedError('acessToken is required') })
   })
 })
