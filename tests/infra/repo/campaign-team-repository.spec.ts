b1ab73dd511237a193b0d6eb221490b4f3c55bf4
Yorí Galisteu <yorigcsdev@gmail.com>
2023-04-27 15:54:16 -0300

    test: should calls and save CampaignTeamRepository correctly

---
+++ tests/infra/repo/campaign-team-repository.spec.ts
@@ -0,0 +1,31 @@
+import { CampaignTeamRepository } from '@/infra/repo'
+
+import { prismaMock } from '@/tests/infra/repo/mocks'
+import { type CampaignTeam } from '@prisma/client'
+
+describe('CampaignTeamRepository', () => {
+  let campaignTeam: CampaignTeam
+  let sut: CampaignTeamRepository
+  const date = new Date()
+
+  beforeAll(() => {
+    campaignTeam = { id: 'any_id', image: 'any_img', nikkes: ['any_nikke'], power: '1000', stage: '10', uploaderId: 'any_uploader_id', createdAt: date, updatedAt: date }
+    prismaMock.campaignTeam.create.mockResolvedValue(campaignTeam)
+  })
+  beforeEach(() => {
+    sut = new CampaignTeamRepository()
+  })
+
+  it('should calls save without error', async () => {
+    const promise = sut.save(campaignTeam)
+    await expect(promise).resolves.not.toThrow()
+    expect(prismaMock.campaignTeam.create).toHaveBeenCalledWith({ data: campaignTeam })
+  })
+
+  it('should throw an error if save throws', async () => {
+    const error = new Error('Error to save campaign team')
+    prismaMock.campaignTeam.create.mockRejectedValue(error)
+    const promise = sut.save(campaignTeam)
+    await expect(promise).rejects.toThrow(error)
+  })
+})
