3bf8d8792e97724361fe036284bcf4833bece82e
Yorí Galisteu <yorigcsdev@gmail.com>
2023-05-02 12:34:23 -0300

    fix: typo in secret

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -3,7 +3,7 @@ export const env = {
   tokenSecret: process.env.TOKEN_SECRET ?? '',
   s3: {
     accessKey: process.env.S3_ACESS_KEY_ID ?? '',
-    secret: process.env.S3_SECRETE_ACESS_KEY ?? '',
+    secret: process.env.S3_SECRET_ACESS_KEY ?? '',
     bucket: process.env.S3_BUCKET ?? '',
     region: process.env.S3_REGION ?? ''
   }
