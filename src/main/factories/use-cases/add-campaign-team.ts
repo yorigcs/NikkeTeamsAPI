576178a8942afe28ae07e173ccbc8a426bec4e9f
Yorí Galisteu <yorigcsdev@gmail.com>
2023-05-06 21:43:48 -0300

    feat: create makeAddCampaignTeamUseCase factory

---
+++ src/main/factories/use-cases/add-campaign-team.ts
@@ -0,0 +1,8 @@
+import { type AddCampaignTeam, setupAddCampaingTeam } from '@/domain/use-cases'
+import { makeAwsS3StorageHandler } from '@/main/factories/infra/storage'
+import { makeUUIDHandler } from '@/main/factories/infra/crypto'
+import { makeCampaignTeamRepository } from '@/main/factories/infra/repo'
+
+export const makeAddCampaignTeamUseCase = (): AddCampaignTeam => {
+  return setupAddCampaingTeam(makeAwsS3StorageHandler(), makeUUIDHandler(), makeCampaignTeamRepository())
+}
