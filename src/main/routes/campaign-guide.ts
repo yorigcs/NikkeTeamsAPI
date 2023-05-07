a3fce3848ef96194555099428aeb063dfa0aa420
Yorí Galisteu <yorigcsdev@gmail.com>
2023-05-06 21:46:55 -0300

    feat: implements auth, upload and controller

--- src/main/routes/campaign-guide.ts
+++ src/main/routes/campaign-guide.ts
@@ -1,8 +1,9 @@
 import { type Router } from 'express'
+
 import { auth } from '@/main/middlewares'
+import { adaptMulter as upload, adapterExpressController as adapt } from '@/main/adapters'
+import { makeAddCampaignTeamController } from '@/main/factories/controllers'
 
 export default (router: Router): void => {
-  router.post('/campaign-upload', auth, (_req, res) => {
-    res.send('ok')
-  })
+  router.post('/campaign-upload', auth, upload, adapt(makeAddCampaignTeamController()))
 }
