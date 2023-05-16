9677368f1739f48b0c5b354b528bf486162ebac2
Yorí Galisteu <yorigcsdev@gmail.com>
2023-05-16 13:43:09 -0300

    chore: update libs and readme

--- src/main/index.ts
+++ src/main/index.ts
@@ -3,6 +3,4 @@ import './config/modules-alias'
 import { env } from '@/main/config/env'
 import { app } from '@/main/config/app'
 
-import 'reflect-metadata'
-
 app.listen(env.port, () => { console.log(`Server running at port ${env.port}`) })
