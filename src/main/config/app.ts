f45d93d84190572b3251eb40e15414f11348e626
Yorí Galisteu <yorigcsdev@gmail.com>
2023-05-26 00:59:22 -0300

    refactor: move register multipart to only specific route

--- src/main/config/app.ts
+++ src/main/config/app.ts
@@ -1,11 +1,9 @@
 import Fastify, { type FastifyInstance } from 'fastify'
 import { setupRoutes } from '@/main/config/routes'
-import { setupFastifyMultipart } from '@/main/config/fastifyMultipart'
 import { setupFastifyCookie } from '@/main/config/fastifyCookie'
 
 const app: FastifyInstance = Fastify({ logger: true })
 setupFastifyCookie(app)
-setupFastifyMultipart(app)
 setupRoutes(app)
 
 export { app }
--- src/main/routes/campaign-team.ts
+++ src/main/routes/campaign-team.ts
@@ -1,9 +1,10 @@
 
 import { auth } from '@/main/middlewares'
-import { adapterFastifyController as adapt } from '@/main/adapters'
+import { adapterFastifyController as adapt, adaptMultipart as upload } from '@/main/adapters'
 import { makeAddCampaignTeamController } from '@/main/factories/controllers'
 import { type FastifyInstance } from 'fastify'
 
 export default async (app: FastifyInstance): Promise<void> => {
-  app.post('/campaign-upload', { preHandler: [auth] }, adapt(makeAddCampaignTeamController()))
+  void app.register(import('@fastify/multipart'), { attachFieldsToBody: true })
+  app.post('/campaign-upload', { preHandler: [auth, upload] }, adapt(makeAddCampaignTeamController()))
 }
