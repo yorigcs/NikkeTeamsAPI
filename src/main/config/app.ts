49a32589d275a22dcb65639f5f1e0a419fbd3c92
Yorí Galisteu <yorigcsdev@gmail.com>
2023-05-28 20:28:57 -0300

    feat: implements setupRateLimit

--- src/main/config/app.ts
+++ src/main/config/app.ts
@@ -1,8 +1,10 @@
 import Fastify, { type FastifyInstance } from 'fastify'
 import { setupRoutes } from '@/main/config/routes'
 import { setupFastifyCookie } from '@/main/config/fastifyCookie'
+import { setupRateLimit } from './fastifyRateLimit'
 
 const app: FastifyInstance = Fastify({ logger: true })
+setupRateLimit(app)
 setupFastifyCookie(app)
 setupRoutes(app)
 
