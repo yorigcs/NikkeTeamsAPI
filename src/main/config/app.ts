107c61c2f6aacc82b863fe03691946597d9d430e
Yorí Galisteu <yorigcsdev@gmail.com>
2023-05-28 21:42:15 -0300

    feat: setup fastify cors

--- src/main/config/app.ts
+++ src/main/config/app.ts
@@ -2,8 +2,10 @@ import Fastify, { type FastifyInstance } from 'fastify'
 import { setupRoutes } from '@/main/config/routes'
 import { setupFastifyCookie } from '@/main/config/fastifyCookie'
 import { setupRateLimit } from './fastifyRateLimit'
+import { setupFastifyCors } from './fastifyCors'
 
 const app: FastifyInstance = Fastify({ logger: true })
+setupFastifyCors(app)
 setupRateLimit(app)
 setupFastifyCookie(app)
 setupRoutes(app)
